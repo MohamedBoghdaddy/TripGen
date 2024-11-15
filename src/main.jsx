6a7780fd2995ebf188b4886956fc761e267fe52a
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2024-11-15 17:48:41 +0200

    Added routing logic and configured routes for different components

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // Make sure this points to your CSS file
+import "./index.css"; 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CreateTrip from './create-trip/index.jsx'
+import Header from "./components/custom/Header.jsx";
+import Hero from "./components/custom/Hero.jsx";
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>
+  },
+  {
+path:'/create-trip',
+    element:<CreateTrip/>
 
+  }
+])
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Header/>
+    <Hero/>
+    
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
