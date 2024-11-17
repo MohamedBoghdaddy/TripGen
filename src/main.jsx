9effe3a73aa8b038a8d8483e12e6e085d18705e1
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2024-11-17 17:25:14 +0200

    feat: Integrate Header, Hero, Features, Pricing, and Footer into main application Details: Imported and integrated the Header, Hero, Features, Pricing, and Footer components into the main app. Set up the structure for smooth scrolling and page routing.

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,25 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; 
+import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import CreateTrip from './create-trip/index.jsx'
+import CreateTrip from "./create-trip/index.jsx";
 import Header from "./components/custom/Header.jsx";
-import Hero from "./components/custom/Hero.jsx";
+
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:<App/>
+    path: "/",
+    element: <App />
   },
   {
-path:'/create-trip',
-    element:<CreateTrip/>
+    path: "/create-trip",
+    element: <CreateTrip />
+  },
+]);
 
-  }
-])
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header/>
-    <Hero/>
-    
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
